0dc4cc3d64399612c06d333420384e586e3c844b
wiltonnunnes <will_nunnes@hotmail.com>
2021-10-27 20:42:03 -0300

    Commit feito em 27/10/2021

--- src/Component/VideoSlider.js
+++ src/Component/VideoSlider.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import MultiProgress from './MultiProgress';
+import styles from './VideoSlider.module.css';
 
 class VideoSlider extends React.Component {
 
@@ -12,6 +13,13 @@ class VideoSlider extends React.Component {
   render() {
     return (
       <div>
+        <input 
+          type="range" 
+          min="0" 
+          max="100" 
+          step="1" 
+          className={styles.inputRange}
+        />
         <MultiProgress 
           backgroundColor="#525865" 
           elements={[
